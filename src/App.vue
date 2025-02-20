4914c1cd0fb0e80dbb40032d5e4a6b58d95acdf6
Evan Yan <evan.yan@constructor.io>
2025-02-19 16:44:48 -0800

    Init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AutocompleteBundledUi />
+  </div>
+</template>
+
+<script>
+import AutocompleteBundledUi from './components/AutocompleteBundledUi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AutocompleteBundledUi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AutocompleteUi.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="autocomplete-ui">
+      <CioAutocomplete v-bind="args" />
+    </div>
+</template>
+
+<script>
+import { CioAutocomplete } from '@constructor-io/constructorio-ui-autocomplete';
+
+export default {
+    components: {
+        CioAutocomplete,
+    },
+    data() {
+        return {
+            args: {
+                "apiKey": "key_JjeChOYODg7w5dYg",
+                "onSubmit": (submitEvent) => console.dir(submitEvent),
+                "autocompleteClassName": "cio-autocomplete full-example-autocomplete-styles",
+                "placeholder": "What can we help you find?",
+                "advancedParameters": {
+                    "displaySearchSuggestionImages": true,
+                    "displaySearchSuggestionResultCounts": true,
+                    "numTermsWithGroupSuggestions": 6
+                },
+                "sections": [
+                    {
+                    "indexSectionName": "Search Suggestions",
+                    "numResults": 8,
+                    "displaySearchTermHighlights": true
+                    },
+                    {
+                    "indexSectionName": "Products",
+                    "numResults": 6,
+                    "displaySearchTermHighlights": true
+                    }
+                ],
+                "zeroStateSections": [
+                    {
+                    "podId": "bestsellers",
+                    "type": "recommendations",
+                    "numResults": 6
+                    }
+                ]
+            }
+        }
+    }
+};
+</script>
+
+<style>
+</style>
